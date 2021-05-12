9adf00efe8d350d69e70a95075528229f144ce71
llx-08 <2596671364@qq.com>
2021-05-12 22:29:46 +0800

    通识课写的

--- classDefine.py
+++ classDefine.py
@@ -1,8 +1,37 @@
+class Stack(object):  # 实现栈，后面符号栈需要使用
+
+    def __init__(self):
+        # 创建空列表实现栈
+        self.__list = []
+
+    def is_empty(self):
+        # 判断是否为空
+        return self.__list == []
+
+    def push(self, item):
+        # 压栈，添加元素
+        self.__list.append(item)
+
+    def pop(self):
+        # 弹栈，弹出最后压入栈的元素
+        if self.is_empty():
+            return
+        else:
+            return self.__list.pop()
+
+    def top(self):
+        # 取最后压入栈的元素
+        if self.is_empty():
+            return
+        else:
+            return self.__list[-1]
+
+
 class symbol:
 
     def __init__(self, s_type, s_id, s_name):
         self.symbol_type = s_type
-        self.symbol_id   = s_id
+        self.symbol_id = s_id
         self.symbol_name = s_name
 
     def printSymbol(self):
@@ -24,17 +53,17 @@ class symbol:
 class LR_1_item:
 
     def __init__(self, id, left, right, next_ss, dot_pos):
-        self.item_id            = id
-        self.leftside           = left
-        self.rightside          = right
+        self.item_id = id
+        self.leftside = left
+        self.rightside = right
         self.next_search_symbol = next_ss
-        self.dot_position       = dot_pos
+        self.dot_position = dot_pos
 
     def equal_item(self, i):
         if self.item_id != i.item_id or self.leftside != i.leftside \
                 or self.next_search_symbol != i.next_search_symbol \
-                or len(self.rightside) != len(i.rightside)\
-                or self.dot_position != i.dot_position :
+                or len(self.rightside) != len(i.rightside) \
+                or self.dot_position != i.dot_position:
             return False
 
         for symbol_Ind in range(len(self.rightside)):
@@ -54,19 +83,91 @@ class LR_1_item:
                 print(" |dot| ", end="")
             print(self.rightside[i].symbol_name, end="")
 
-        print("\n下一个搜索的符号为: "+self.next_search_symbol)
+        print("\n下一个搜索的符号为: " + self.next_search_symbol)
 
         return
 
+    def if_dot_pos_last(self):
+        return self.dot_position == len(self.rightside)
+
+    def symbol_next_dot(self):
+        if self.if_dot_pos_last():
+            return -1
+        else:
+            return self.rightside[self.dot_position]
 
 
-def splitGrammarIntoProduction(grammar):
+class LR_1_item_set:
 
-    terminal_set    = []
-    non_terminal_set = []
+    def __init__(self, id):
+        self.item_set_id = id
+        self.item_set = []
+
+    def item_exist(self, item):
+        for i in self.item_set:
+            if i.equal_item(item):
+                return True
+
+        return False
+
+    def add_item(self, item):
+        if self.item_exist(item):
+            return -1
+        else:
+            self.item_set.append(item)
+
+
+# action_table: {
+#     first : s   :操作类型
+#     second: num :操作编号
+# }
+def build_action_table():
+    pass
+
+
+def build_goto_table():
+    pass
+
+
+def goto(item_set, symbol, item_set_id):
+
+    j = LR_1_item_set(item_set_id)
+
+    for item in item_set:
+        if item.symbol_next_dot() != symbol:
+            continue
+        if not j.item_exist(item):
+            j.add_item(item)
+    return j
+
+
+def generateItemSet(LR_item_set):
+    # 初始化
+    item_set_id = 0
+    begin_item_set = LR_item_set(item_set_id)
 
     for g in grammar:
-        left  = g[0]
+        left = g[0]
+        right = g[1].split()
+        next_ss = 1  # 怎么确定下一个搜索符号？
+        dot_pos = 0
+        temp_item = LR_1_item(id, left, right, next_ss, dot_pos)
+
+        begin_item_set.add_item(temp_item)
+
+    flag = 1
+    while flag:
+        item_set_id += 1
+        
+
+def splitGrammarIntoProduction(grammar):
+    terminal_set     = []
+    non_terminal_set = []
+    production       = {}
+    count            = 0
+
+    for g in grammar:
+        left = g[0]
         if left not in terminal_set:
             terminal_set.append(left)
 
@@ -85,29 +186,43 @@ def splitGrammarIntoProduction(grammar):
     for n in non_terminal_set:
         print(n, end=" ")
 
-    return terminal_set, non_terminal_set
-
-def buildSymbolSet(terminal_set, non_terminal_set):
-    terminal_symbol_set     = []
-    non_terminal_symbol_set = []
-
-    for t in terminal_set:
-        symbol()
-
-def generateItemSet(LR_item_set):
-
-    # 初始化
-
     for g in grammar:
-        left    = g[0]
-        right   = g[1].split()
-        next_ss =
-        dot_pos = 0
-        temp_item = LR_1_item(id, left, right, next_ss, dot_pos)
+        production[count] = [g[0], g[1].split()]
+        count += 1
+
+    return terminal_set, non_terminal_set, production
 
 
-    for lr_item in LR_item_set:
-        
+# LR 分析器
+def LR_analysis(action_table, goto_table, production, input_buffer):  # 符号输入串
+    symbol_stack = Stack()  # 符号栈
+    status_stack = Stack()  # 状态栈
+
+    status_stack.push(0)
+    # 初始化
+    input_buffer.append("$")
+
+    curr_input = 0
+
+    while ~status_stack.is_empty() and len(input_buffer) > curr_input:
+
+        # 将
+        symbol_stack.push(input_buffer[curr_input])
+        curr_input += 1
+
+        action_move = action_table[status_stack.top(), input_buffer[curr_input]]
+
+        if action_move[0] == "s":
+            status_stack.push(action_move[1])
+        elif action_move[0] == "r":
+            id = action_move[1]
+
+        elif action_move[0] == "a":
+            return "Grammar Analysis Success"
+        else:
+            return "Grammar Analysis Failed"
+
+
 
 
 
@@ -119,5 +234,4 @@ grammar = [
     ["C", "d"]
 ]
 
-t_set, non_t_set = splitGrammarIntoProduction(grammar)
-
+t_set, non_t_set, production = splitGrammarIntoProduction(grammar)
