ba6b0a20e2af656013713c5c68db8a5023490e63
llx-08 <2596671364@qq.com>
2021-05-15 00:51:49 +0800

    生成follow集完成

--- LL1/class_methodDefine.py
+++ LL1/class_methodDefine.py
@@ -66,18 +66,72 @@ def get_grammarAndProduction(grammar):
     return n_set, t_set, production
 
 
-def build_predict_table(all_first_set):
-
+def build_predict_table(all_first_set, all_follow_set, production):
     for first_set in all_first_set:
-
-
         pass
 
 
-def grammar_analyse(input_buffer, symbol_stack, predict_table):
-    pass
+def error_solver(first_set, follow_set, type):
+    if type == 1:  # 栈顶的终结符与当前输入符不匹配
+        pass
+
+    elif type == 2:  # 栈顶为非终结符A，面临的输入符为a，但分析表中M[A,a]为空
+        pass
 
 
-def draw_grammar_tree():
+def grammar_analyse(n_set, t_set, input_buffer, predict_table, first_set, follow_set):
+    # 初始化
+    used_production = []  # 用到的产生式，按顺序排列，用于构建语法树
+
+    symbol_stack = Stack()
+    symbol_stack.push("$")
+    symbol_stack.push("S")
+
+    input_buffer.append("$")
+
+    curr_input_index = 0
+
+    # 补一个判断语句
+    while curr_input_index < len(input_buffer):
+
+        input_symbol = input_buffer[curr_input_index]
+        top_symbol = symbol_stack.top()
+
+        # 如果X是终结符，且X=a≠＄
+        if top_symbol in t_set and top_symbol == input_symbol and top_symbol != "@":
+            symbol_stack.pop()  # X出栈
+            curr_input_index += 1  # 读取下一个输入符号
+
+        elif top_symbol in t_set and top_symbol != input_symbol and top_symbol != "@":
+            error_solver(first_set, follow_set, 1)  # X ≠ a则报错
+
+        elif top_symbol in n_set:  # X是非终结符，查M[X，a]表
+            if predict_table[top_symbol][input_symbol] == "ERR":  # 若M[X，a]=error，则报错
+                error_solver(first_set, follow_set, 2)
+
+            elif predict_table[top_symbol][input_symbol] is not None:  # 有产生式，非空
+                used_production.append(predict_table[top_symbol][input_symbol])
+
+                symbol_stack.pop()
+
+                rightside = predict_table[top_symbol][input_symbol][1]  # 产生式右部
+
+                for r in reversed(rightside):
+                    symbol_stack.push(r)
+
+        elif top_symbol == "$" and input_symbol == "$":
+            return "Grammar Analyse Success!", used_production
+
+        else:
+            error_solver(first_set, follow_set, 2)
+
+
+def draw_grammar_tree(used_prediction):
+
+    for prediction in used_prediction:
+        pass
+
+
+def loop_drawing():
 
     pass
--- LL1/first_follow.py
+++ LL1/first_follow.py
@@ -14,8 +14,8 @@ def get_first_set(t_symbol_set, n_symbol_set, production):
             if p[0] == symbol:  # p[0]找到了对应的产生式 p[0] -> Y1Y2
                 # X是非终结符，且有产生式X → Y1 Y2…… Yk
 
-                print("symbol", end=": ")
-                print(symbol)
+                # print("symbol", end=": ")
+                # print(symbol)
 
                 if symbol not in all_first_set.keys():  # 还未创建symbol的first集
                     all_first_set[symbol] = []
@@ -25,7 +25,8 @@ def get_first_set(t_symbol_set, n_symbol_set, production):
 
                 elif p[1][0] in n_symbol_set:  # Y1是非终结符,将FIRST(Y1)–ε中的所有符号加入到FIRST(X)
                     if p[1][0] not in all_first_set.keys():  # 还未创建需要加入的的first集
-                        remain_add.append([symbol, p[1][0]])
+                        if [symbol, p[1][0]] not in remain_add:
+                            remain_add.append([symbol, p[1][0]])
                     else:
                         for s in all_first_set[p[1][0]]:
                             if s != "@" and s not in all_first_set[symbol]:
@@ -47,7 +48,8 @@ def get_first_set(t_symbol_set, n_symbol_set, production):
                     elif flag == 0:
 
                         if p[1][i] not in all_first_set.keys():  # 还未创建需要加入的的first集
-                            remain_add.append([symbol, p[1][i]])
+                            if [symbol, p[1][i]] not in remain_add:
+                                remain_add.append([symbol, p[1][i]])
                         else:
                             for ss in all_first_set[p[1][i]]:
                                 if ss != "@" and ss not in all_first_set[symbol]:
@@ -61,6 +63,13 @@ def get_first_set(t_symbol_set, n_symbol_set, production):
     print("remaining")
     print(remain_add)
 
+    for i in range(len(remain_add)):  # 排序，保证添加first集的顺序
+        for j in range(i, len(remain_add)):
+            if remain_add[i][1] == remain_add[j][0]:
+                temp = remain_add[i]
+                remain_add[i] = remain_add[j]
+                remain_add[j] = temp
+
     for r in remain_add:
         for t_symbol in all_first_set[r[1]]:
             if t_symbol != "@" and t_symbol not in all_first_set[r[0]]:
@@ -71,12 +80,14 @@ def get_first_set(t_symbol_set, n_symbol_set, production):
 
 def test_first_set():
     grammar = [
-        ["S", "B A"],
-        ["A", "B S"],
-        ["A", "d"],
-        ["B", "a A"],
-        ["B", "b S"],
-        ["B", "c"]
+        ["E", "T E'"],
+        ["E'", "+ T E'"],
+        ["E'", "@"],
+        ["T", "F T'"],
+        ["T'", "* F T'"],
+        ["T'", "@"],
+        ["F", "( E )"],
+        ["F", "id"]
     ]
 
     n_set, t_set, production = \
@@ -85,12 +96,99 @@ def test_first_set():
 
     first_set = get_first_set(t_set, n_set, production=production)
     print("first set test result:")
-    print(first_set)
+    for i in first_set:
+        print(i, end="   ")
+        print(first_set[i])
+
+    return first_set, n_set, t_set, production
 
 
-def get_follow_set():
-    pass
+def get_follow_set(t_symbol_set, n_symbol_set, production, first_set):
+    all_follow_set = {}
+
+    remain_add = []  # 在最后将所有符号重新加入: 被添加，添加来源
+
+    # for t in t_symbol_set:
+    #     all_follow_set[t] = ["@"]
+
+    for n in n_symbol_set:
+        all_follow_set[n] = ["$"]
+
+    for n in n_symbol_set:
+        for p in production:
+            if p[0] == n:  # 此处n为PPT中A
+                rightside = p[1]
+
+                for i in range(len(p[1]) - 1):
+                    B = rightside[i]
+                    beta = rightside[i + 1]
+                    # print("A -> B BETA : ", )
+                    # print(n, end="  -> ")
+                    # print(B, end=" ")
+                    # print(beta)
+
+                    # 对于A→αB, 将FOLLOW(A)放入FOLLOW(B)中
+                    if i+1 == len(p[1])-1 and beta in n_symbol_set:  # beta为最后一个，且为非终结符
+
+                        for follow_symbol in all_follow_set[n]:
+                            if follow_symbol not in all_follow_set[beta]:
+                                all_follow_set[beta].append(follow_symbol)
+
+                        remain_add.append([beta, n])
+
+                        # print("new beta")
+                        # print(all_follow_set[beta])
+
+                        if B in n_symbol_set:
+                            # print("in B")
+                            # print(B)
+                            for pro in production:  # A→αBβ且β→ε
+                                # print("NULL TEST OUTER LOOP")
+                                # print(pro[0], end=" -> ")
+                                # print(pro[1])
+
+                                if pro[0] == beta and pro[1][0] == "@":
+                                    # print("NULL TEST")
+                                    # print(pro[0])
+                                    # print(pro[1])
+
+                                    remain_add.append([B, n])
+
+                                    for f_a in all_follow_set[n]:
+                                        if f_a not in all_follow_set[B]:
+                                            all_follow_set[B].append(f_a)
+
+                    # 对于A→αBβ的产生式，则将FIRST(β)–ε放入FOLLOW(B)
+                    if B in n_symbol_set:
+                        # print("IN FIRST LOOP:  B: ", end="")
+                        # print(B)
+                        # remain_add.append([B, beta, "first"]) first集合是不会修改的，不需要remain
+                        # print("symbol need append")
+                        for first_symbol in first_set[beta]:
+                            # print(first_symbol)
+                            if first_symbol not in all_follow_set[B] and first_symbol != "@":
+                                all_follow_set[B].append(first_symbol)
+
+    # print(remain_add)
+    for i in range(len(remain_add)):
+        for re in reversed(remain_add):
+            for f_symbol in all_follow_set[re[1]]:
+                if f_symbol not in all_follow_set[re[0]]:
+                    all_follow_set[re[0]].append(f_symbol)
+
+    return all_follow_set
 
 
-if __name__ == '__main__':
-    test_first_set()
+def test_follow_set():
+    first_set, n_set, t_set, production = test_first_set()
+
+    follow_set = get_follow_set(t_set, n_set, production, first_set)
+
+    print("follow set test result:")
+    for f in follow_set:
+        print(f, end=": ")
+        print(follow_set[f])
+
+
+# if __name__ == '__main__':
+#     test_follow_set()
